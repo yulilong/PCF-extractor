b256e08cfb6373afe8a71912e650f05d3f415838
yulilong <yulilong222q@outlool.com>
2015-07-17 14:42:59 +0800

    package update

--- tmp.rb
+++ tmp.rb
@@ -1,7 +1,15 @@
 
 str = "hula,,,https://rubygems.org/gems/hula,Page not found"
-
-am = str.strip.split(',');
-p  am
+ww = "1.2.0"
+ee = "1.2"
+new = ww.split('.');
+old = ee.split('.');
+if new.size == 3 and old.size == 2
+	if new[0] == old[0] and new[1] = old[1] and new[2] = '0'
+		p "mark"
+	end
+end
+am = str.split(',');
+p  am.class
 hh = ''
-p ",,#{hh},,Page not found,\n"
+p ",,#{hh},,Page not found,\n" + ''
